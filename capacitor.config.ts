dd7663a313810e6d7e8f3ade54620f8b50b7c4fc
tshimpagilaDavid <davidtshipangila@gmail.com>
2025-01-31 13:52:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Calendar-Challenge',
+  webDir: 'www'
+};
+
+export default config;
